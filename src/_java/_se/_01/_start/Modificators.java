0156bfc4059d93c19fe1d555aa2a4527ddf63a7e
Hook10 <sapasask@gmail.com>
2020-01-17 11:48:26 +0600

    first lesson

---
+++ src/_java/_se/_01/_start/Modificators.java
@@ -0,0 +1,20 @@
+//package _java._se._01._start;
+//
+//
+//public class Modificators {
+//    public static void main(String[] args) {
+//        AboutJava obj = new AboutJava();
+//        String s1 = obj.getAboutJava();
+//        String s2 = obj.info();
+//    }
+//}
+//
+//Class AboutJava{
+//    public String getAboutJava(){
+//        return info();
+//        }
+//
+//private String info(){
+//        return"About Java.";
+//        }
+//}
---
+++ src/_java/_se/_01/_start/variablesExample/VariablesExample.java
@@ -0,0 +1,19 @@
+package _java._se._01._start.variablesExample;
+
+public class VariablesExample {
+    boolean statusOn;
+    double javaVar = 2.34;
+
+    public static void main(String[] args) {
+        int itemsSold = 04;
+        double salary = 1.234e3;
+        float itemCost = 11.0f;
+        int i = 0xFd45, k$;
+        double _interestRate;
+    }
+
+    public void javaMethod() {
+        long simpleVar = 1_000_000_000_000L;
+        byte byteVar2 = 123;
+    }
+}
